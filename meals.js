eb6b6247816da1557f19774625fa267883711783
usfalidiate <51782274+usfalidiate@users.noreply.github.com>
2024-11-11 13:06:15 +1100

    Update meals.js

--- meals.js
+++ meals.js
@@ -1,11 +1,19 @@
 // Array of food items with their corresponding calories
 
 export const meals = [
-  { name: "Oatmeal", calories: 300 },
-  { name: "Apple", calories: 95 },
-  { name: "Chicken Sandwich", calories: 400 },
-  { name: "Salad", calories: 150 },
-  { name: "Pasta", calories: 500 },
   { name: 'Large Skim Cap', calories: 101 },
-  // Add more food items as needed
+  { name: "Deb Chicken Salad", calories: 300 },
+  { name: "50g Trail Mix", calories: 250 },
+  { name: "Skim Coffee Sachet", calories: 40 },
+  { name: "125g Chicken", calories: 150 },
+  { name: "misc50", calories: 50 },
+  { name: "misc50", calories: 50 },
+  { name: "misc100", calories: 100 },
+  { name: "misc100", calories: 100 },
+  { name: "misc200", calories: 200 },
+  { name: "misc200", calories: 200 },
+  { name: "misc500", calories: 500 },
+
+  
+  // Add more food items as needed   { name: "blank", calories: 100 },
 ];
